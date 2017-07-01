bd083904c62e327c69cdad9478259b1a697ea419
ricgu8086 <ricgu8086@gmail.com>
2017-07-01 13:17:45 +0200

    Reformat code to my prefered style.

--- src/PID.cpp
+++ src/PID.cpp
@@ -3,19 +3,26 @@
 using namespace std;
 
 /*
-* TODO: Complete the PID class.
-*/
+ * TODO: Complete the PID class.
+ */
 
-PID::PID() {}
-
-PID::~PID() {}
-
-void PID::Init(double Kp, double Ki, double Kd) {
+PID::PID()
+{
 }
 
-void PID::UpdateError(double cte) {
+PID::~PID()
+{
 }
 
-double PID::TotalError() {
+void PID::Init(double Kp, double Ki, double Kd)
+{
+}
+
+void PID::UpdateError(double cte)
+{
+}
+
+double PID::TotalError()
+{
 }
 
